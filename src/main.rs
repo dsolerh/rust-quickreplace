754de0ad7a2c55b6e96966486423864b1f970e85
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-14 11:30:31 +0200

    add fn to parse the cli arguments and print usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use text_colorizer::*;
+
 #[derive(Debug)]
 struct Arguments {
     target: String,
@@ -7,5 +10,30 @@ struct Arguments {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+}
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 5 {
+        print_usage(&args[0]);
+        eprintln!("{} wrong number of arguments: expected 4 but got {}.",
+                  "Error".red().bold(), args.len()-1);
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[1].clone(),
+        replacement: args[2].clone(),
+        filename: args[3].clone(),
+        output: args[4].clone(),
+    }
+}
+
+fn print_usage(app_name: &str) {
+    eprintln!("{} - change ocurrences of one string into another",
+              app_name.green());
+    eprintln!("Usage: {} <target> <replacement> <INPUT> <OUTPUT>",
+              app_name);
 }
